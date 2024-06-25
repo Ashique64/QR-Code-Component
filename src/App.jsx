9eb026d992ab8929e0d4375da7bca44b1efbcc83
Ashique <ashique9496@gmail.com>
2024-06-25 10:09:01 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <div className="card">
+                    <div className="items">
+                        <img src="\public\qr-code.png" alt="" />
+                        <h5>Improve your front-end skills by building projects</h5>
+                        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
